aa86bf988c26164b46e046eef17f611644a50351
dionisiusyose <dionisiusyose11@gmail.com>
2020-11-04 17:18:21 +0700

    add accounts

---
+++ JWTWebAPI_Dion/ASPNetWebClient_Dion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNetWebClient_Dion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
